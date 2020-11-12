dbdd68cafcf73def4a74bf24088602a740615e3a
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-12 16:37:01 +0100

    feat: findByEmail & EmployeeRequest

--- src/main/java/com/example/authspring/Repository/EmployeeRepository.java
+++ src/main/java/com/example/authspring/Repository/EmployeeRepository.java
@@ -4,4 +4,5 @@ import com.example.authspring.entities.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+    Employee findByEmail(String email);
 }
