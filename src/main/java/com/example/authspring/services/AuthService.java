68da68da15a6cd032b14e4a7231bbfbe8da4151b
Pablo Sabater <pabloosabaterr@gmail.com>
2020-11-12 16:32:35 +0100

    feat: SignIn, verifications

--- src/main/java/com/example/authspring/services/AuthService.java
+++ src/main/java/com/example/authspring/services/AuthService.java
@@ -4,6 +4,7 @@ import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
 import com.example.authspring.Repository.EmployeeRepository;
+import com.example.authspring.dto.EmployeeRequest;
 import com.example.authspring.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -15,14 +16,45 @@ public class AuthService {
 
     public String createEmployee(Employee employee){
         try{
+            if(employee.getEmail() == null){
+                return "You must write an email";
+            }else if(employee.getPassword() == null){
+                return "You must write a password";
+            }else if(employee.getName() == null){
+                return "You must write a name";
+            }
             employeeRepository.save(employee);
             Algorithm algorithm = Algorithm.HMAC256("superSecret");
             String token = JWT.create()
-                    .withIssuer(employee.getName())
+                    .withClaim("email", employee.getEmail())
                     .sign(algorithm);
             return token;
         }catch (JWTCreationException e){
             return "Invalid Signing configuration / Couldn't convert Claims.";
+        }
+    }
+
+    public String SignInUser(EmployeeRequest employee){
+        try {
+            if(employee.getEmail() == null || employee.getPassword() == null){
+                return "You must write an email and a password";
+            }
+            Employee foundEmployee = employeeRepository.findByEmail(employee.getEmail());
+            if(foundEmployee == null){
+                return null;
+            }
+
+            if(!foundEmployee.getPassword().equals(employee.getPassword())){
+                return null;
+            }
+
+            Algorithm algorithm = Algorithm.HMAC256("superSecret");
+            String token = JWT.create()
+                    .withClaim("email", employee.getEmail())
+                    .sign(algorithm);
+            return token;
+        }catch(JWTCreationException e){
+            return null;
         }
     }
 }
